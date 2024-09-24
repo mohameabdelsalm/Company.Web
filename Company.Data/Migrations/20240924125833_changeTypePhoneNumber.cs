1cca25d0b63a6b3f27a7e684d858bdad23561c27
Mohamed abdelsalam <abdelsalamm789@gmail.com>
2024-09-24 16:02:21 +0300

    TestUploadFile

---
+++ Company.Data/Migrations/20240924125833_changeTypePhoneNumber.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Company.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeTypePhoneNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 11);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PhoneNumber",
+                table: "employees",
+                type: "int",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
