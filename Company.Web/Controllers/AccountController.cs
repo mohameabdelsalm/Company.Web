183c39042fcdfd814760b765057289fc426a900b
Mohamed abdelsalam <abdelsalamm789@gmail.com>
2024-10-03 03:26:08 +0300

    Layout SignUp

---
+++ Company.Web/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using Company.Data.Entites;
+using Company.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public IActionResult SginUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SginUp(SignupViewModel input)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = input.Email.Split("@")[0],
+                    FristName=input.FristName,
+                    LastName = input.LastName,
+                    Email = input.Email,
+                    IsActive=true
+                };
+               var result=await _userManager.CreateAsync(user,input.Password);
+
+                if (result.Succeeded) 
+                {
+                    return RedirectToAction("SignIn");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("",error.Description);
+                }
+            }
+            return View(input);
+        }
+    }
+}
---
+++ Company.Web/Models/SignupViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.Web.Models
+{
+    public class SignupViewModel
+    {
+       [Required(ErrorMessage ="FirstName is required")]
+        public string FristName { get; set; }
+
+        [Required(ErrorMessage = "LastName is required")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage ="Invalid Format for Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [RegularExpression(@"^(?=(.*[A-Z]){1})(?=(.*[a-z]){1})(?=(.*\d){1})(?=(.*\W){1})(?!.*(.)\1\1).{8,}$", ErrorMessage = "Password must meet the complexity requirements.")]
+
+        public string Password { get; set; }
+        [Required(ErrorMessage = "ConfirmPassword is required")]
+        [Compare(nameof(Password),ErrorMessage = "ConfirmPassword is not match password")]
+        public string ConfirmPassword { get; set; }
+        public bool IsAgree { get; set; }
+
+    }
+}
